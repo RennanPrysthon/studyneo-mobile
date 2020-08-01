ccb80b267c5f614dda6bfd7db023d20bbac6a5b5
Filipe Melo <filipemelo032@gmail.com>
2020-08-01 18:14:27 -0300

    [Bug-fix]Homescreen styles set

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,27 +2,30 @@ import styled from 'styled-components/native';
 
 export const Container = styled.ScrollView`
   flex: 1;
-  padding: 20px;
-  background-color: #FEFEFE;
+  padding: 0px;
+  background-color: #fefefe;
 `;
 
 export const AreaTitle = styled.Text`
   font-size: 22px;
   font-weight: 600;
-`
+  padding-left: 20px;
+`;
 
 export const Item = styled.View`
   margin-bottom: 20px;
+  margin-top: 20px;
 `;
 
 export const List = styled.ScrollView`
-  padding: 10px;
+  padding-top: 10px;
 `;
 
 export const ListItem = styled.TouchableOpacity`
   width: 88px;
   height: 88px;
-  margin-right: 20px;
+  margin-right: 8px;
+  margin-left: 20px;
   border-radius: 5px;
   justify-content: center;
   align-items: center;
