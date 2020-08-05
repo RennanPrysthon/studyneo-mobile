5609fe92c272f8863e2b0f14bad78a53ab209ae0
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-08-05 18:54:14 -0300

    Diminuindo tamanho do componente de exibicao de questao

--- src/components/Alternative/styles.ts
+++ src/components/Alternative/styles.ts
@@ -3,9 +3,11 @@ import styled from "styled-components/native";
 
 interface Props {
   showBorder?: boolean;
+  backgroundColor?: string;
 }
 
 export const Container = styled(Pressable) <Props>`
+
   border-color: ${({ showBorder }) => showBorder === true ? '#000' : '#ddd'};
   border-width: 2px;
 
