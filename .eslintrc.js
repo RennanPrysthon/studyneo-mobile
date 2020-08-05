f63d62b4e50c6d5a908f23b4135e578597e8e5cf
Rennan Prysthon <rennandelcastillo@gmail.com>
2020-08-05 16:23:04 -0300

    Adicionando babel plugin import

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,13 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  settings: {
+    "import/resolver": {
+      "typescript": {},
+      "babel-plugin-root-import": {
+        "rootPathPrefix": "~",
+        "rootPathSuffix": "src"
+      }
+    }
+  }
 };
